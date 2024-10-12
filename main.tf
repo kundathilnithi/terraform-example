b203a449cad576346e22d8e59841515cb196f010
kundathilnithi <kundathil.nithi@gmail.com>
2024-10-12 13:05:38 +0530

    update code

--- main.tf
+++ main.tf
@@ -2,23 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           =  "ami-0866a3c8686eaeeba" 
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-   user_data = <<-EOF
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+}
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size = 1
+  max_size = 10
 
-  user_data_replace_on_change = true
-
-  tags = {
-  Name = "terraform-example"
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
-
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
