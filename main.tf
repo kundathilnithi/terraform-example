07881cde706929ba70d76c8a413b91669ec7641a
kundathilnithi <kundathil.nithi@gmail.com>
2024-10-12 15:35:05 +0530

    update code

--- main.tf
+++ main.tf
@@ -26,9 +26,11 @@ resource "aws_autoscaling_group" "example" {
     id      = aws_launch_template.example.id
     version = "$Latest"
   }
-  min_size = 1
+  min_size = 2
   max_size = 10
   vpc_zone_identifier  = data.aws_subnets.default.ids
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   tag {
     key                 = "Name"
@@ -47,4 +49,83 @@ resource "aws_security_group" "instance" {
   }
 
 
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "vpc-id" {
   value       = data.aws_subnets.default.ids
   description = "VPC ID's"
 }
+
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
